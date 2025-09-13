bbe9a05194d5ddc8a1ba1bde664e2d78db9c9fad
Ceesar99 <dishusamuelprosper@gmail.com>
2025-09-13 12:24:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py - Keep your keys safe (add to .gitignore)
 
-TELEGRAM_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN_HERE'  # From BotFather
-ALPHA_VANTAGE_KEY = 'YOUR_ALPHA_VANTAGE_API_KEY_HERE'  # Free from alphavantage.co
+TELEGRAM_TOKEN = '8414457542:AAEpbDNb52CJ928EfW1cUT8kR1ZsS8fM6h0'  # From BotFather
+ALPHA_VANTAGE_KEY = 'LA1OBXRGG71CFCLW'  # Free from alphavantage.co
 
 # Trading pair and settings
 PAIR = 'EURUSD'  # Example forex pair
